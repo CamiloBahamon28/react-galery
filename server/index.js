e09bcc113a219548c6186b586029a5399485fd4e
CamiloBahamon28 <camilobahamon280301@gmail.com>
2021-07-03 01:25:39 -0500

    2 commit, se crean las rutas de las imagenes y se desarrollo algo de la ruta upload, falta por terminar dicha ruta

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 import express from 'express'
-import { asap } from 'rsvp'
+import fileUpload from 'express-fileupload'
 
+//importando las rutas
 import indexRoutes from './routes/index.routes' 
+import imagesRoutes from './routes/images.routes' 
 
 const app = express()
 
 app.set('port', process.env.PORT || 4000)
 
+app.use(fileUpload({
+    tempFileDir: '/temp'//para guardar los archivos en una carpeta temporal
+}))
+
+//utilizando las rutas
 app.use(indexRoutes)
+app.use(imagesRoutes)
 
 app.listen(app.get('port'))
 console.log('Server Port', app.get('port'));
--- server/routes/images.routes.js
+++ server/routes/images.routes.js
@@ -1,3 +1,19 @@
 import {Router} from 'express'
 
 const router = Router()
+
+router.post('/api/images/upload', async (req, res)=>{
+    
+    const { file } = req.files;
+    console.log(file);
+
+    return res.json('received')
+})//subir una imagen al servidor
+
+router.get('/api/images', async (req, res)=>{})//para listar todas la imagenes guardadas en la BD y en donde esta guardada
+
+router.get('/api/images/:id', async (req, res)=>{})//para obtener la info de una sola imagen
+
+router.delete('/api/images/:id', async (req, res)=>{})//eliminar una imagen apartir de un id
+
+export default router
